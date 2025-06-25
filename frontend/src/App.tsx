ca2f72f97f3c3783bc6f4b8f6b79fc2d92313010
Goutam Dogri <dogrig507@gmail.com>
2025-06-25 21:33:23 +0530

    add my name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Hii this is Goutam.</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
